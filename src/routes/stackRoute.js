5ce0de94df9f0edc8d2cf4eb8940c4fb252d3ad4
felipeslopes <felipesl.lopes@hotmail.com>
2023-08-12 18:57:34 -0300

    alterando titulo do header

--- src/routes/stackRoute.js
+++ src/routes/stackRoute.js
@@ -26,42 +26,57 @@ export default function StackRoute() {
             }}
         >
 
-            <Stack.Screen name="Home" component={Home}
+            <Stack.Screen
+                name="Home"
+                component={Home}
                 options={{ headerShown: false }}
             />
 
-            <Stack.Screen name="ThePlanets" component={ThePlanets}
+            <Stack.Screen
+                name="ThePlanets"
+                component={ThePlanets}
                 options={{ title: 'Planetas do Sistema Solar' }}
             />
 
-            <Stack.Screen name="DetailsPlanets" component={DetailsPlanets} />
-
-            <Stack.Screen name="Planet" component={Planet} />
-
-            <Stack.Screen name="PhotoSpace" component={PhotoSpace}
-                options={{
-                    title: 'Foto Astronômica do dia',
-                }}
+            <Stack.Screen
+                name="DetailsPlanets"
+                component={DetailsPlanets}
             />
 
-            <Stack.Screen name="HomeRover" component={HomeRover} options={{ title: 'Rover' }} />
+            <Stack.Screen
+                name="Planet"
+                component={Planet}
+            />
 
-            <Stack.Screen name="PhotoEarth" component={PhotoEarth}
+            <Stack.Screen
+                name="PhotoSpace"
+                component={PhotoSpace}
+                options={{ title: 'Foto Astronômica do dia' }}
+            />
+
+            <Stack.Screen
+                name="HomeRover"
+                component={HomeRover}
+                options={{ title: 'Imagens Rovers em Marte' }}
+            />
+
+            <Stack.Screen
+                name="PhotoEarth"
+                component={PhotoEarth}
                 options={{ title: 'Foto Policromática da Terra' }}
             />
 
-            <Stack.Screen name="DetailsPhoto" component={DetailsPhoto}
-                options={{
-                    title: 'Detalhes da imagem',
-                }}
+            <Stack.Screen
+                name="DetailsPhoto"
+                component={DetailsPhoto}
+                options={{ title: 'Detalhes da imagem' }}
             />
 
             <Stack.Screen
                 name="BottonRoute"
                 component={BottonRoute}
                 options={{
-                    title: sharedValue,
-                    headerTitleAlign: 'center',
+                    title: sharedValue, headerTitleAlign: 'center',
                 }}
             />
 
